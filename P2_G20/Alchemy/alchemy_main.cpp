72dcf11d2ee931086d63367cf92e6d0094dcff0b
guillermosanchezsanc <guillermosanchezsanc@S3-10>
2017-04-25 10:59:11 +0200

    first commit

---
+++ P2_G20/Alchemy/alchemy_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "alchemy.cpp"
+#include <vector>
+#include <set>
+#include <unordered_map>
+
+
+int main() {
+
+	return 0;
+}
---
+++ Project5/Project5/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <set>
+#include <iterator>
+#include <algorithm>
+#include <string>
+#include <fstream>
+
+
+ifstream fentrada("test04.dat");
+//UNION
+int main() {
+
+	return 0;
+}
